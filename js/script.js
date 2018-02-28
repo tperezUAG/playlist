8b6a9675eee8d882eaff2888c69fffe9cea65f4d
Thomas Perez <thomasperez@uagateway.org>
2018-02-28 23:38:40 +0000

    Added the function that displays the playlist and added some css

--- js/script.js
+++ js/script.js
@@ -25,22 +25,22 @@ var mySong = {
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		title:"24K Magic",
+		artist:"Bruno Mars",
+		mp3Url:"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		title:"Sir Duke",
+		artist:"Stevie Wonder",
+		mp3Url:"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		imageUrl:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		title:"Sorry",
+		artist:"Justin Bieber",
+		mp3Url:"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		imageUrl:"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -49,20 +49,17 @@ var myPlayList = [
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  
-
-
-
+	displayList()
 });
 
 function displayList(){
-
-
-  
+	for (var i = 0; i < myPlayList.length; i++) {
+		$("#songs").append("<div class=\"col-md-12\"><div class=\"thumbnail\"><h4 class=\"text-center\">" + "  " + myPlayList[i].title + "--" + myPlayList[i].artist + "  " + "</h4><img class=\"img-responsive\" src=\"" + myPlayList[i].imageUrl + "\" /><br><a href=\"" + myPlayList[i].mp3Url + "\"><button class=\"btn btn-outline\">Play</button></a></div></div>");
+	}
 }
 
 function clearList(){
-  
+	$("#songs").html("");
   
   
 }
